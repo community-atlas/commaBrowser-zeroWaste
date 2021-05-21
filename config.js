7c5a38cdc7da42761d6fe2bc4584dbe0ddc65d66
Peter Brownell <greenman@the-organization.com>
2021-05-21 12:14:17 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,28 @@
+/**
+ * 
+ * 
+ * 
+ */
+
+
+var GA_MEASUREMENT_ID = "UA-168836142-1"
+var CONFIG = {
+    "map": {
+        "editorUrl": "https://coda.io/d/dvdo5WMIQlF",
+        "editorContact": "mailto:test@example.com",
+        "commaJSONUrl": "https://comensi.github.io/atlases/zwlpt-DETxemFzyY.geojson",
+        "mapBoxToken": "pk.eyJ1IjoiZ3JlZW5tYW4yMyIsImEiOiJjazBrMmMwMG8wYmppM2N0azdqcnZuZzVjIn0.jpODNTgb9TIxZ6yhZKnTvg",
+        "mapId": "mapbox/streets-v11",
+        "startView": "map",
+        "showDetail": true,
+        "lang": "it",
+        "logos" : [
+            {
+               src: "commaBrowser/browser/images/logo-erasmusleft_en.jpg",
+               alt: "Erusmus",
+               href: null,
+            },
+        
+        ]
+    }
+}
