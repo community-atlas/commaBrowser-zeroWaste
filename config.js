7269c0e489a68c48708b4c131859152afe62b3c3
Greenman <greenman@the-organization.com>
2021-10-04 20:28:37 +0100

    Editor url updated

--- config.js
+++ config.js
@@ -5,17 +5,17 @@
  */
 
 
-var GA_MEASUREMENT_ID = "UA-168836142-1"
+var GA_MEASUREMENT_ID = ""
 var CONFIG = {
     "map": {
-        "editorUrl": "https://coda.io/d/dvdo5WMIQlF",
+        "editorUrl": "https://coda.io/d/dDETxemFzyY",
         "editorContact": "mailto:test@example.com",
         "commaJSONUrl": "https://comensi.github.io/atlases/zwlpt-DETxemFzyY.geojson",
         "mapBoxToken": "pk.eyJ1IjoiZ3JlZW5tYW4yMyIsImEiOiJjazBrMmMwMG8wYmppM2N0azdqcnZuZzVjIn0.jpODNTgb9TIxZ6yhZKnTvg",
         "mapId": "mapbox/streets-v11",
         "startView": "map",
         "showDetail": true,
-        "lang": "it",
+        "lang": "pt",
         "logos" : [
             {
                src: "commaBrowser/browser/images/logo-erasmusleft_en.jpg",
